dd84a3fa37f1e5a1900603373a1c91bdd8b25b95
André Restivo <andre.restivo@gmail.com>
2015-07-17 16:01:27 +0100

    Organize Imports

--- src/schedule/LectureFactory.java
+++ src/schedule/LectureFactory.java
@@ -1,11 +1,5 @@
 package schedule;
 
-import java.util.Date;
-
-import infrastructure.Room;
-import instance.Instance;
-import people.Teacher;
-import schedule.Schedule.WEEKDAY;
 
 public class LectureFactory {
 	public static Lecture createLecture(Schedule schedule, int number) {
